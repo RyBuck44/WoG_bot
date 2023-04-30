2512ac22c74e394a6ac9c6918b923451c78c8413
RyBuck44 <bucklerryan2@gmail.com>
2023-04-29 20:59:33 -0400

    hid token, linked with server, added indication that bot is on

--- bot.py
+++ bot.py
@@ -1,4 +1,22 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-import os 
+import os
+
+secret_code = os.getenv('WoG_TOKEN')
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+class db(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False
+
+    async def on_ready(self):
+        await tree.sync(guild=discord.Object(id=1100490695309017168))
+        self.synced = True
+        print('Bot is Online')
+
+
+bot = db()
+tree = app_commands.CommandTree(bot)
